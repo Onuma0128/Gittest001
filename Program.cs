572392d024c3bde6ca32412eca912920bbaf66a2
k023g1012@g.neec.ac.jp <k023g1012@g.neec.ac.jp>
2023-10-20 12:09:35 +0900

    プルリクエストするために変更

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, VisualStadio Git!");
-Console.WriteLine("Hello, New Git!");
+Console.WriteLine("Hello, New branch!");
 
